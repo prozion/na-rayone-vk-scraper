fa68be1567fe5ba276e2e185af2ecf6ca519ce8c
Denis Shirshov <denis.shirshov@gmail.com>
2021-04-21 15:16:03 +0300

    7.3

--- www/updater.php
+++ www/updater.php
@@ -1,7 +1,7 @@
 <?php
 
 function load_page($filename, $writedir) {
-  $digitalocean_url = "http://188.166.159.222/na_rayone/";
+  $digitalocean_url = "http://188.166.159.222/pages/na_rayone/";
   $handle = fopen($digitalocean_url . $filename, "r");
   $content = stream_get_contents($handle);
   file_put_contents($writedir . $filename, $content);
